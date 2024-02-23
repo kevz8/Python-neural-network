3d3a5fc368bcf7cc0a8b794b8bb050f285e247d5
kevz8 <kevz8811@gmail.com>
2024-02-22 23:16:29 -0800

    Included more training data

--- Neural-net.py
+++ Neural-net.py
@@ -1,29 +1,24 @@
 import numpy as np
 
-r = 0.05
-i = 1.5
-
 def sigmoid(i, deriv = False):
     if deriv:
         return i * (1 - i)
     return 1/(1 + np.exp(-i))
 
 # Input
-X = np.array([[0,0,1], 
-              [0,1,1],
-              [1,0,1],
-              [1,1,1]])
+X = np.array([[0, 0, 0],
+              [0, 0, 1],
+              [0, 1, 0],
+              [0, 1, 1],
+              [1, 0, 0],
+              [1, 0, 1],
+              [1, 1, 0],
+              [1, 1, 1]])
 
 # Output
-y = np.array([[0], [1], [1], [0]])
+y = np.array([[1], [0], [0], [1], [0], [1], [1], [0]])
 
-w1 = 2 * np.random.random(size = (3, 1)) - 1
-w2 = 2 * np.random.random(size = (3, 1)) - 1
-w = [0.8]
-
-b = 0
-
-# Backpropogation
+# Propogation
 
 class Neural_net(object):
     def __init__(self):
@@ -35,8 +30,11 @@ class Neural_net(object):
         self.W1 = 2 * np.random.random(size = (self.input_size, self.hidden_size)) - 1
         self.b1 = np.zeros((1, self.hidden_size))
 
-        self.W2 = 2 * np.random.random(size = (self.hidden_size, self.output_size)) - 1
-        self.b2 = np.zeros((1, self.output_size))
+        self.W2 = 2 * np.random.random(size = (self.hidden_size, self.hidden_size)) - 1
+        self.b2 = np.zeros((1, self.hidden_size))
+
+        self.W3 = 2 * np.random.random(size = (self.hidden_size, self.output_size)) - 1
+        self.b3 = np.zeros((1, self.output_size))
 
     def feedForward(self, X):
         #forward propagation
@@ -44,22 +42,31 @@ class Neural_net(object):
         self.a1 = sigmoid(z1)
 
         z2 = np.dot(self.a1, self.W2) + self.b2
-        output = sigmoid(z2)
+        self.a2 = sigmoid(z2)
+
+        z3 = np.dot(self.a2, self.W3) + self.b3
+        output = sigmoid(z3)
         return output
     
-    def backpropagation(self, X, y, r = 0.05):
+    def backpropagation(self, X, y, r = 0.01):
         output = self.feedForward(X)
         a1 = self.a1
+        a2 = self.a2
         error = y - output
 
         output_delta = error * sigmoid(output, deriv = True)
-        self.W2 += np.dot(a1.T, output_delta) * r
-        self.b2 += np.sum(output_delta, axis = 0, keepdims = True) * r
+        self.W3 += np.dot(a2.T, output_delta) * r
+        self.b3 += np.sum(output_delta, axis = 0, keepdims = True) * r
 
-        hidden_error = np.dot(output_delta, self.W2.T)
-        hidden_delta = hidden_error * sigmoid(a1, deriv = True)
-        self.W1 += np.dot(X.T, hidden_delta) * r
-        self.b1 += np.sum(hidden_delta, axis = 0, keepdims = True) * r
+        hidden2_error = np.dot(output_delta, self.W3.T)
+        hidden2_delta = hidden2_error * sigmoid(a2, deriv = True)
+        self.W2 += np.dot(a1.T, hidden2_delta) * r
+        self.b2 += np.sum(hidden2_delta, axis = 0, keepdims = True) * r
+
+        hidden1_error = np.dot(hidden2_delta, self.W2.T)
+        hidden1_delta = hidden1_error * sigmoid(a1, deriv = True)
+        self.W1 += np.dot(X.T, hidden1_delta) * r
+        self.b1 += np.sum(hidden1_delta, axis = 0, keepdims = True) * r
 
 
 neural_network = Neural_net()
@@ -76,4 +83,3 @@ print("W2:", neural_network.W2, "b2:", neural_network.b2)
 final_output = neural_network.feedForward(X)
 print("Output after training:")
 print(final_output)
-
